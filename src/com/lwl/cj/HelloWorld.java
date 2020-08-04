d9ba19c5d1eb0565f8edaebf303d72f768e7e880
Pavani <kumar0080@gmail.com>
2020-08-03 21:53:43 -0500

    Add a multiplication of table logic

--- src/com/lwl/cj/HelloWorld.java
+++ src/com/lwl/cj/HelloWorld.java
@@ -4,5 +4,9 @@ public class HelloWorld {
 
 		public static void main(String[] args) {
 			System.out.println("Welcome to Java world!");
+			int num = 19;
+			for(int i=1;i<=10;i++) {
+				System.out.println(num + " * "+i+" = "+(num*i));
+			}
 		}
 }
